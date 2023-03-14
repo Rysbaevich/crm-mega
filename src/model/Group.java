6df7820319dd732fe99033c00d9b77310692c7b9
Нурдөөлөт <r.nurdoolot@gmail.com>
2023-03-14 11:19:48 +0600

    Реализовано StudentDaoImplV2.java методы save() и findById()

--- src/model/Group.java
+++ src/model/Group.java
@@ -1,8 +1,15 @@
 package model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.time.LocalTime;
 import java.util.List;
 
+@Getter
+@Setter
+@ToString
 public class Group extends BaseEntity {
     private String name;
     private String room;
@@ -10,52 +17,4 @@ public class Group extends BaseEntity {
     private Mentor mentor;
     private Course course;
     private List<Student> students;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRoom() {
-        return room;
-    }
-
-    public void setRoom(String room) {
-        this.room = room;
-    }
-
-    public LocalTime getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(LocalTime startTime) {
-        this.startTime = startTime;
-    }
-
-    public Mentor getMentor() {
-        return mentor;
-    }
-
-    public void setMentor(Mentor mentor) {
-        this.mentor = mentor;
-    }
-
-    public Course getCourse() {
-        return course;
-    }
-
-    public void setCourse(Course course) {
-        this.course = course;
-    }
-
-    public List<Student> getStudents() {
-        return students;
-    }
-
-    public void setStudents(List<Student> students) {
-        this.students = students;
-    }
 }
